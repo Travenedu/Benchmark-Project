14f8274c05e82755b7c8ba42d00763850523e124
Traven Reese <82552048+Travenedu@users.noreply.github.com>
2023-02-06 11:36:48 -0500

    Implemented Floating point benchmark
    In this commit floating_point_benchmark was implement with code similar to integer _operation_benchmark. Also integer _operation_benchmark was updated with some minor tweaks.

--- Integer_operation_benchmark.cpp
+++ Integer_operation_benchmark.cpp
@@ -2,25 +2,34 @@
 #include <vector>
 #include <string>
 
+#include <cmath>
 #include <chrono>
 
-using namespace std;
+using namespace std:: chrono;
 
 int main(){
     
-    int result; 
+    int result = 0; 
+    auto start = std::chrono::steady_clock::now();
 
     for (int i; i < 1010; i++){
         result += i;
     }
 
-    for (int i; i < 1010; i++){
+    for (int i; i < pow(5*10,9); i++){
         result *= i;
     }
 
-    for (int i; i < 1010; i++){
+    for (int i; i < pow(2*10,9); i++){
         result /= i;
     }
+
+    auto end = std::chrono::steady_clock::now();
+
+    std::chrono::duration<double> elapsed_seconds = end - start;
+
+    
+    std::cout << "Elapsed time: " << elapsed_seconds.count() << "s\n";
 
     return 0;
     
