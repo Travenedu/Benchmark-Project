07a6706737873a7e66bcab5786333c9c2972a78f
Traven Reese <82552048+Travenedu@users.noreply.github.com>
2023-02-07 20:55:24 -0500

    Memory Benchmark Implementation
    Implement the code for the memory benchmark

--- memory_benchmark.cpp
+++ memory_benchmark.cpp
@@ -2,10 +2,36 @@
 #include <vector>
 #include <string>
 
-using namespace std;
+#include <cmath>
+#include <chrono>
+
+using namespace std:: chrono;
 
 int main(){
-    cout <<  "Hello World";
+    
+    //Initializing variables
+    int x;
+    
+
+    //starting timer
+    auto start = std::chrono::steady_clock::now();
+
+    //Memory Operation
+    int array[5000000000] = { 0 };
+    for (int i; i < pow(5*10, 9); i++){
+        array[i] = i;
+        x = array[i];
+    }
+
+    //End timer
+    auto end = std::chrono::steady_clock::now();
+
+    //Compute duration of benchmark
+    std::chrono::duration<double> elapsed_seconds = end - start;
+
+    //Print results of benchmark    
+    std::cout << "Elapsed time: " << elapsed_seconds.count() << "s\n";
+
     return 0;
     
 }
